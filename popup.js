53045c59590752f5a287ca74d3f28f0a244cc8d4
Ilia_Korol <iliyaking46@gmail.com>
2020-04-20 15:38:16 +0300

    Add link to G-Translate and make some code improvements

--- popup.js
+++ popup.js
@@ -1,67 +1,67 @@
 'use strict';
 
-const getTranlate = (data) => {
+const getTranslate = (data) => {
     if (!data) return;
-    const text = typeof data === 'string' ? data : data[0]
+    const text = typeof data === 'string' ? data : data[0];
     if (!text) return;
 
     const word = text.split(' ')[0];
-    const { sl, tl } = /^[а-яА-Я\d ]+$/.test(word) ? { sl: 'ru', tl: 'en' } : { sl: 'en', tl: 'ru' };
+    const {sl, tl} = /^[а-яА-Я\d ]+$/.test(word) ? {sl: 'ru', tl: 'en'} : {sl: 'en', tl: 'ru'};
     const url = `https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&dj=1&sl=${sl}&tl=${tl}&q=${text}`
     fetch(url)
         .then(data => data.json())
         .then((data) => {
-            const { sentences } = data;
+            const {sentences} = data;
             if (sentences && sentences.length > 0) {
-                const original = sentences.map(({ orig }) => orig).join('\n')
-                const translation = sentences.map(({ trans }) => trans).join(' ')
+                const original = sentences.map(({orig}) => orig).join('\n');
+                const translation = sentences.map(({trans}) => trans).join(' ');
                 setTranslation({original, translation, sl, tl})
             }
         })
-}
+};
 
 const setTranslation = ({original, translation, sl, tl}) => {
-    const originalTextBlock = document.getElementById("original")
+    const originalTextBlock = document.getElementById('original');
     originalTextBlock.innerHTML = original;
     originalTextBlock.style.display = 'block';
 
-    const translatedTextBlock = document.getElementById("translation")
+    const translatedTextBlock = document.getElementById('translation');
     translatedTextBlock.innerHTML = translation;
     translatedTextBlock.style.display = 'block';
 
-    document.getElementById("copy").onclick = () => {
-      copyToClipboard(translation)
-    }
+    const copyButton = document.getElementById('copy')
+    copyButton.onclick = () => copyToClipboard(translation);
+    copyButton.style.display = 'flex';
 
-    const moreLink = document.getElementById("more");
-    moreLink.href = `https://translate.google.com/?#${sl}/${tl}/${original}`
+    const moreLink = document.getElementById('more');
+    moreLink.href = `https://translate.google.com/#${sl}/${tl}/${original}`;
     moreLink.style.display = 'inline-block';
-}
+};
 
 // copy to clipboard
 const copyToClipboard = str => {
-  const el = document.createElement('textarea');
-  el.value = str;
-  el.setAttribute('readonly', '');
-  el.style.position = 'absolute';
-  el.style.left = '-9999px';
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
 };
 
 // translate from selection
 document.addEventListener('DOMContentLoaded', () => {
-  chrome.tabs.executeScript(
-    {code: "window.getSelection().toString().trim();"},
-    getTranlate
-  );
+    chrome.tabs.executeScript(
+        {code: 'window.getSelection().toString().trim();'},
+        getTranslate,
+    );
 });
 
 document.getElementById('search-form').onsubmit = (e) => {
     e.preventDefault();
     const value = document.getElementById('text-input').value;
-    getTranlate(value);
+    getTranslate(value);
     document.getElementById('text-input').value = '';
-}
+};

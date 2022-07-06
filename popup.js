8bd793ff6046e6635a6ed2b4ae3c25dc784d33ea
Ilia_Korol <iliyaking46@gmail.com>
2022-07-06 18:47:31 +0300

    redesign *.*

--- popup.js
+++ popup.js
@@ -1,21 +1,78 @@
 'use strict';
 
-const translateBox = document.getElementById('translate-box');
-const originalTextBlock = document.getElementById('original');
+const swapButton = document.getElementById('swap-button');
+const [source, translated] = document.getElementsByClassName('lang')
 const translatedTextBlock = document.getElementById('translation');
-const moreLink = document.getElementById('more');
 const copyButton = document.getElementById('copy-btn');
-const footer = document.getElementsByClassName('footer')[0];
+const more = document.getElementsByClassName('more')[0];
 const textInput = document.getElementById('text-input');
+const SOURCES = {
+    EN: 'en',
+    RU: 'ru'
+}
+let currentSource = SOURCES.EN
 
-const getTranslate = async (data) => {
+swapButton.onclick = swapLanguages
+
+copyButton.onclick = copyToClipboard;
+
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+        const [injectionResult] = await chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: () => window.getSelection().toString().trim(),
+        });
+        const { result = '' } = injectionResult
+        textInput.value = result;
+        startTranslate(result);
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+textInput.addEventListener(
+    'keyup',
+    debounce((event) => {
+        startTranslate(event.target.value);
+    })
+);
+
+function startTranslate(data) {
     if (!data) return;
     const text = typeof data === 'string' ? data : data[0];
     if (!text) return;
+    checkTextSize(text)
+    const { sl, tl } = /[а-яёА-ЯЁ]+/.test(text) ? { sl: SOURCES.RU, tl: SOURCES.EN } : { sl: SOURCES.EN, tl: SOURCES.RU };
+    if (sl != currentSource) {
+        currentSource = sl;
+        swapLanguageInfo()
+    }
+    getTranslate({ text, sl, tl })
+}
 
-    const { sl, tl } = /[а-яёА-ЯЁ]+/.test(text) ? { sl: 'ru', tl: 'en' } : { sl: 'en', tl: 'ru' };
+function swapLanguages() {
+    if (swapButton.classList.contains('rotate')) {
+        swapButton.classList.remove('rotate')
+    } else {
+        swapButton.classList.add('rotate')
+    }
+    swapLanguageInfo()
 
-    footer.href = `https://translate.google.com/#${sl}/${tl}/${encodeURI(text)}`;
+    textInput.value = translatedTextBlock.innerText;
+    translatedTextBlock.innerText = ''
+    checkTextSize(textInput.value)
+
+    const newSource = currentSource === SOURCES.EN ? SOURCES.RU : SOURCES.EN
+    getTranslate({ text: textInput.value, sl: newSource, tl: currentSource })
+    currentSource = newSource;
+}
+
+async function getTranslate({ text, sl, tl }) {
+    if (!text) return;
+
+    more.href = `https://translate.google.com/#${sl}/${tl}/${encodeURI(text)}`;
 
     const url = `https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&dj=1&sl=${sl}&tl=${tl}&q=${encodeURI(text)}`;
 
@@ -29,73 +86,64 @@ const getTranslate = async (data) => {
         if (sentences && sentences.length > 0) {
             const original = sentences.map(({ orig }) => orig).join(' ');
             const translation = sentences.map(({ trans }) => trans).join('');
-            setTranslation({ original, translation, sl, tl });
+            setTranslation({ translation });
         }
     } catch (error) {
         console.log(error)
         const original = `Something went wrong, click link bellow for fix<br/><a href=${url} target="_blank">enter captha at google translate...</a>`;
-        setTranslation({ original, translation: '' });
+        setTranslation({ translation: original, error: true });
     }
 };
 
-const setTranslation = ({ original, translation, sl, tl }) => {
-    originalTextBlock.innerHTML = original;
-    translatedTextBlock.innerText = translation;
-
-    translateBox.style.display = 'block';
-
-    if (sl && tl) {
-        copyButton.onclick = () => copyToClipboard(translation);
-        copyButton.style.display = 'flex';
-
-        moreLink.href = `https://translate.google.com/#${sl}/${tl}/${original}`;
-        moreLink.style.display = 'inline-block';
+function setTranslation({ translation, error }) {
+    if (error) {
+        translatedTextBlock.innerHTML = translation;
+    } else {
+        translatedTextBlock.innerText = translation;
     }
 };
+
+function swapLanguageInfo() {
+    const temp = source.textContent
+    source.textContent = translated.textContent
+    translated.textContent = temp
+}
+
+function checkTextSize(text) {
+    if (text.length > 40 && !textInput.classList.contains('small')) {
+        textInput.classList.add('small')
+    }
+
+    if (text.length <= 40 && textInput.classList.contains('small')) {
+        textInput.classList.remove('small')
+    }
+}
 
 // copy to clipboard
-const copyToClipboard = (str) => {
-    const el = document.createElement('textarea');
-    el.value = str;
-    el.setAttribute('readonly', '');
-    el.style.position = 'absolute';
-    el.style.left = '-9999px';
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-};
+// function copyToClipboard(str) {
+//     const el = document.createElement('textarea');
+//     el.value = str;
+//     el.setAttribute('readonly', '');
+//     el.style.position = 'absolute';
+//     el.style.left = '-9999px';
+//     document.body.appendChild(el);
+//     el.select();
+//     document.execCommand('copy');
+//     document.body.removeChild(el);
+// };
 
-document.addEventListener("DOMContentLoaded", async () => {
-    try {
-        const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+function copyToClipboard() {
+    const text = translatedTextBlock.innerText;
+    navigator.clipboard.writeText(text)
+}
 
-        const [injectionResult] = await chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            function: () => window.getSelection().toString().trim(),
-        });
-        const { result = '' } = injectionResult
-        textInput.value = result;
-        getTranslate(result);
-    } catch (error) {
-        console.log(error)
-    }
-});
-
-const debounce = (func) => {
+function debounce(func) {
     let timer;
     return (...args) => {
         const next = () => func(...args);
         if (timer) {
             clearTimeout(timer);
         }
-        timer = setTimeout(next, 300);
+        timer = setTimeout(next, 500);
     };
 }
-
-textInput.addEventListener(
-    'keyup',
-    debounce((event) => {
-        getTranslate(event.target.value);
-    })
-);

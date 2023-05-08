8ce4da188294501995342171f51077f41d46adc1
Ilia_Korol <iliyaking46@gmail.com>
2023-05-08 11:39:18 +0300

    change styles and source to auto

--- background.js
+++ background.js
@@ -8,7 +8,7 @@ chrome.contextMenus.onClicked.addListener(
   ({ selectionText, menuItemId }) => {
     if (!selectionText || menuItemId !== CONTEXT_MENU_ID) return;
 
-    const { sl, tl } = /[а-яёА-ЯЁ]+/.test(selectionText) ? { sl: 'ru', tl: 'en' } : { sl: 'en', tl: 'ru' };
+    const { sl, tl } = /[а-яёА-ЯЁ]+/.test(selectionText) ? { sl: 'ru', tl: 'en' } : { sl: 'auto', tl: 'ru' };
 
     const url = `https://translate.google.com/#${sl}/${tl}/${encodeURI(selectionText)}`;
     chrome.tabs.create({ url: url });
